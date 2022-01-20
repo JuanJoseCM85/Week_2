cb3077837bfbcbd6cf3f39a40489d2dfb7a91157
JuanJoseCM85 <juanjosecm85@gmail.com>
2022-01-20 12:53:38 +0100

    Commit modificaciones library.ts

--- Typescript/library.ts
+++ Typescript/library.ts
@@ -54,10 +54,26 @@ export class Library{
         return this.books.length;
     }
 
+    public findByAuthor(autor:string):Book[]
+    {
+        let libroEncontrado:Book[] = [];
+
+
+        for(let lib of this.books){
+            if ( lib.getAuthor() == autor){
+                console.log("Entra");
+                libroEncontrado.push(lib);
+            }
+        }
+
+        return libroEncontrado;
+
+    }
+
 }
 
 let libro1 = new Book("El señor de los granillos",344,"ES03503IB2","Un autor conocido","LibroAbierto");
-let libro2 = new Book("El señor de los granillos2",3442,"ES03503IB22","Un autor conocido2","LibroAbierto2");
+let libro2 = new Book("El señor de los granillos2",3442,"ES03503IB22","Un autor conocido3","LibroAbierto2");
 let libro3 = new Book("El señor de los granillos3",3443,"ES03503IB23","Un autor conocido3","LibroAbierto3");
 
 let arrayLibros:Book[];
@@ -71,6 +87,13 @@ console.log(biblio.toString());
 console.log("Numero de libros: ");
 console.log(biblio.getNumberOfBooks());
 
+let arrayLibrosAutor = biblio.findByAuthor("Un autor conocido3");
+
+for(let lib2 of arrayLibrosAutor)
+{
+    console.log(lib2.getTitle());
+    
+}
 
 
 
